6738c018ec96580e713fe70ca22b382dc94d1f7a
Todd Birchard <toddbirchard@gmail.com>
2019-06-02 14:13:28 -0400

    initial google cloud commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from application.bot import Bot
 from config import username, password, testRoom, blabroom, acleeRoom
 
 
-if __name__ == "__main__":
+def main():
     Bot.easy_start([testRoom, acleeRoom, blabroom],
                    username,
                    password)
