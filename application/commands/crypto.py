a05ac2c5e7c225d207fffdb6233e8a5653041854
Todd <todd.birchard@fanduel.com>
2019-11-13 13:58:00 -0500

    stock & crypto cleanup

--- application/commands/crypto.py
+++ application/commands/crypto.py
@@ -4,13 +4,11 @@ import requests
 def get_crypto_price(message):
     """Get crypto price for provided ticker label."""
     req = requests.get(url=message)
-    x = req.json()
-    y = x["result"]["price"]
-    z = y["change"]
-    last = str(y["last"])
-    high = str(y["high"])
-    low = str(y["low"])
-    percentage = z["percentage"]*100
+    prices = req.json()["result"]["price"]
+    last = prices["last"]
+    high = prices["high"]
+    low = prices["low"]
+    percentage = prices["change"]['percentage']*100
     msg = "Currently at $" + last \
         + ", high today of $" + high \
         + ", low of $" + low \
--- application/commands/stock.py
+++ application/commands/stock.py
@@ -5,13 +5,14 @@ from config import Config
 def get_stock_price(symbol):
     params = {'token': Config.iex_api_key}
     req = requests.get('https://sandbox.iexapis.com/stable/stock/' + symbol + '/quote', params=params)
-    print(req.status_code)
     if req.status_code == 200:
-        price = req.json()['latestPrice']
-        companyName = req.json()["companyName"]
-        high = req.json()["high"]
-        low = req.json()["low"]
-        change = round(req.json()["ytdChange"], 4)
-        message = f"{companyName} current price of ${price}. Percent change of {change}"
-        return message
-    return f'theres no such company as {symbol} :@'
+        price = req.json().get('latestPrice', None)
+        company_name = req.json().get("companyName", None)
+        # high = req.json()["high"]
+        # low = req.json()["low"]
+        if price and company_name:
+            change = round(req.json()["ytdChange"], 2)
+            price = round(price, 2)
+            message = f"{company_name} current price of ${price}. Percent change of {change}"
+            return message
+    return f'There\'s no such company as {symbol} :@'
