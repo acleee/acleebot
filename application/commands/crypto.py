31cce19c81052420fddb8305b023504a5cdc0d26
Todd Birchard <toddbirchard@gmail.com>
2020-02-09 22:58:11 -0500

    crypto rounding

--- application/commands/crypto.py
+++ application/commands/crypto.py
@@ -5,13 +5,13 @@ def get_crypto_price(symbol, message):
     """Get crypto price for provided ticker label."""
     req = requests.get(url=message)
     prices = req.json()["result"]["price"]
-    last = int(prices["last"])
-    high = int(prices["high"])
-    low = int(prices["low"])
+    last = prices["last"]
+    high = prices["high"]
+    low = prices["low"]
     percentage = prices["change"]['percentage']*100
     percentage = '%.2f' % (percentage)
     symbol = symbol.upper()
-    msg = f'{symbol}: Currently at ${last}. \
-            High today of ${high}, low of ${low}. \
+    msg = f'{symbol}: Currently at ${last:.2f}. \
+            High today of ${high:.2f}, low of ${low:.2f}. \
             Change of {percentage}%'
     return msg
