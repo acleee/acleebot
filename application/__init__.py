ff0b4ed1a3275385230b7d657dccef7e6a9793ca
Todd Birchard <toddbirchard@gmail.com>
2020-04-15 20:24:06 -0400

    Pass rooms as list (fixed bug).

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ def start_bot(room):
     """Initialize bot instance for a single room."""
     print(f'Joining {room}...')
     print(f'commands = {commands}')
-    Bot.easy_start(rooms=room,
+    Bot.easy_start(rooms=[room],
                    name=CHATANGO_USERNAME,
                    password=CHATANGO_PASSWORD,
                    commands=commands)
