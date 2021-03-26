996ca3769999b1dbdc3fa291bc5015cf50531b12
Todd <toddbirchard@gmail.com>
2021-03-25 23:47:41 -0400

    Rap genius lyrics search command.

---
+++ broiestbot/commands/lyrics.py
@@ -0,0 +1,20 @@
+from clients import genius
+from logger import LOGGER
+
+
+def get_song_lyrics(song_title_query: str) -> str:
+    """
+    Search for song lyrics by song title.
+
+    :param song_title_query: Song title to fetch lyrics for.
+    :type song_title_query: str
+    :returns: str
+    """
+    try:
+        song = genius.search_song(song_title_query)
+        if song is not None and song.lyrics() is not None:
+            lyrics = song.lyrics()[0:1800]
+            return lyrics
+        return f"wtf kind of song is `{song_title_query}`"
+    except Exception as e:
+        LOGGER.error(f"Unexpected error while searching for song: `{e}`")
