5d82af32a7dad52c633833778718db7d94f5594e
Todd <toddbirchard@gmail.com>
2021-03-28 14:28:28 -0400

    Fix footy preferred time formats, add LIGA fixtures.

--- broiestbot/commands/__init__.py
+++ broiestbot/commands/__init__.py
@@ -3,11 +3,11 @@ from .afterdark import get_redgifs_gif
 from .definitions import get_urban_definition, wiki_summary
 from .embeds import create_instagram_preview
 from .footy import (
+    epl_golden_boot,
     epl_standings,
     footy_live_fixtures,
     footy_predicts_today,
     footy_upcoming_fixtures,
-    golden_boot,
 )
 from .images import (
     fetch_image_from_gcs,
@@ -15,11 +15,11 @@ from .images import (
     random_image,
     subreddit_image,
 )
+from .lyrics import get_song_lyrics
 from .markets import get_crypto, get_stock
 from .misc import blaze_time_remaining, covid_cases_usa, send_text_message
 from .movies import find_imdb_movie
 from .weather import weather_by_location
-from .lyrics import get_song_lyrics
 
 
 def basic_message(message):
