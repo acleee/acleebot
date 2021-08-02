28ea7b88790bf98a0889905aaf241276491ad026
toddbirchard <toddbirchard@gmail.com>
2021-08-01 23:20:26 -0400

    Sort medals by gold.

--- broiestbot/commands/olympics.py
+++ broiestbot/commands/olympics.py
@@ -4,25 +4,26 @@ from emoji import emojize
 
 
 def get_olympic_medals_per_nation() -> str:
+    """Return olympic leaders by number of gold medals"""
     medals_df = pd.read_html(
-        "https://www.espn.com/olympics/summer/2020/medals",
+        "https://www.espn.com/olympics/summer/2020/medals/_/view/overall/sort/gold",
         flavor="bs4",
         attrs={"class": "medals olympics has-team-logos"},
         header=0,
+        index_col=None,
     )
     medals_df = medals_df[0].head(10)
     medals_df.rename(
         columns={
             "Group": "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",
-            "G": f"{emojize(':1st_place_medal:', use_aliases=True)}  ",
-            "S": f"{emojize(':2nd_place_medal:', use_aliases=True)}  ",
-            "B": f"{emojize(':3rd_place_medal:', use_aliases=True)}",
+            "G": f"{emojize(':1st_place_medal:', use_aliases=True)}&nbsp;",
+            "S": f"{emojize(':2nd_place_medal:', use_aliases=True)}&nbsp;",
+            "B": f"{emojize(':3rd_place_medal:', use_aliases=True)}&nbsp;",
         },
         inplace=True,
     )
-    # medals_df.iloc[0] = medals_df.apply(format_country_name, axis=1)
-    return f"\n\n{medals_df.to_string(index=False, header=True, col_space=10, justify='center')}"
+    return f"\n\n{medals_df.to_string(header=True, index=False, col_space=10, justify='center')}"
 
 
 def format_country_name(value: str):
-    return f"{value}&nbsp;&nbsp;"
+    return f"{value}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
