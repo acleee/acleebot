ff08150316981826e43fd40649808c90c7ad82fa
toddbirchard <toddbirchard@gmail.com>
2021-11-16 18:00:00 -0500

    Rename SQL URI variable.

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import DATABASE_ARGS, DATABASE_URI
+from config import DATABASE_ARGS, SQLALCHEMY_DATABASE_URI
 
-engine = create_engine(DATABASE_URI, connect_args=DATABASE_ARGS, echo=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args=DATABASE_ARGS, echo=False)
 Session = sessionmaker(bind=engine, autocommit=True)
 session = Session()
