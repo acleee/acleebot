68a6d5555a7aa1cd240079a8d7288a7db883f223
toddbirchard <toddbirchard@gmail.com>
2021-11-28 10:43:16 -0800

    Abbreviate NFL team locations.

--- broiestbot/commands/nfl/__init__.py
+++ broiestbot/commands/nfl/__init__.py
@@ -7,7 +7,8 @@ from logger import LOGGER
 
 
 def get_live_nfl_games() -> str:
-    """Get summary of all live NFL games, scores and odds.
+    """
+    Get summary of all live NFL games, scores and odds.
 
     :returns: str
     """
@@ -17,15 +18,12 @@ def get_live_nfl_games() -> str:
         games = resp.json().get("results")
         if resp.status_code == 429:
             return emojize(
-                f":warning: yall used the command too much now they tryna charge me smh :warning:"
+                f":warning: y'all used the command too much now they tryna charge me smh :warning:"
             )
         if games:
             game_summaries = "\n\n\n\n"
             for i, game in enumerate(games):
-                summary = f"{game['teams']['away']['team']} <b>({game['scoreboard']['score']['away']})</b> @ {game['teams']['home']['team']} <b>({game['scoreboard']['score']['home']})</b>\n"
-                summary += f"Period {game['scoreboard']['currentPeriod']}, {game['scoreboard']['periodTimeRemaining']}\n"
-                summary += f"Spread {game['odds'][0]['spread']['open']['home']} {game['teams']['home']['abbreviation']}, {game['odds'][0]['spread']['open']['away']} {game['teams']['away']['abbreviation']}\n\n"
-                game_summaries += summary
+                game_summaries += format_live_nfl_game(game)
             return game_summaries
         return emojize(":warning: No live NFL games atm :( :warning:", use_aliases=True)
     except HTTPError as e:
@@ -34,3 +32,19 @@ def get_live_nfl_games() -> str:
         LOGGER.error(f"KeyError while fetching live NFL games: {e}")
     except Exception as e:
         LOGGER.error(f"Unexpected error when fetching live NFL games: {e}")
+
+
+def format_live_nfl_game(game: dict) -> str:
+    """
+    Format live match-up summary between two teams.
+
+    :params dict game: Dictionary summary of a single live NFL fixture.
+
+    :returns: str
+    """
+    # fmt: off
+    summary = f"{game['teams']['away']['abbreviation']} {game['teams']['away']['mascot']} <b>({game['scoreboard']['score']['away']})</b> @ {game['teams']['home']['abbreviation']} {game['teams']['home']['mascot']} <b>({game['scoreboard']['score']['home']})</b>\n"
+    summary += f"Period {game['scoreboard']['currentPeriod']}, {game['scoreboard']['periodTimeRemaining']}\n"
+    summary += f"Spread {game['odds'][0]['spread']['open']['home']} {game['teams']['home']['abbreviation']}, {game['odds'][0]['spread']['open']['away']} {game['teams']['away']['abbreviation']}\n\n"
+    # fmt: on
+    return summary
