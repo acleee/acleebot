af1c0e9f81d3c883f8842ce33f024ffd5c3fc209
Todd Birchard <toddbirchard@gmail.com>
2022-02-05 10:39:14 -0500

    Clean up lyrics lookup.

--- broiestbot/commands/lyrics.py
+++ broiestbot/commands/lyrics.py
@@ -1,3 +1,5 @@
+from emoji import emojize
+
 from clients import genius
 from logger import LOGGER
 
@@ -13,8 +15,16 @@ def get_song_lyrics(song_title_query: str) -> str:
     try:
         song = genius.search_song(song_title_query)
         if song is not None and song.lyrics is not None:
-            lyrics = f"{song.full_title} \n\n {song.lyrics[0:1800]}..."
-            return lyrics
+            lyrics = song.lyrics[:1800].replace("Embed", "")
+            return f"{song.full_title} \n\n {lyrics}..."
         return f"wtf kind of song is `{song_title_query}`"
+    except LookupError as e:
+        LOGGER.error(f"LookupError error while searching for song: `{e}`")
+        return emojize(
+            f":warning: wtf kind of song is `{song_title_query}` :warning:", use_aliases=True
+        )
     except Exception as e:
         LOGGER.error(f"Unexpected error while searching for song: `{e}`")
+        return emojize(
+            f":warning: wtf kind of song is `{song_title_query}` :warning:", use_aliases=True
+        )
