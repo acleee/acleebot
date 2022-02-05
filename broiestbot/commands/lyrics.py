65d9677fe82c0cf50ed0761070606a48d5fea38a
Todd Birchard <toddbirchard@gmail.com>
2022-02-05 10:41:30 -0500

    Clean up lyrics lookup.

--- broiestbot/commands/lyrics.py
+++ broiestbot/commands/lyrics.py
@@ -17,7 +17,9 @@ def get_song_lyrics(song_title_query: str) -> str:
         if song is not None and song.lyrics is not None:
             lyrics = song.lyrics[:1800].replace("Embed", "")
             return f"{song.full_title} \n\n {lyrics}..."
-        return f"wtf kind of song is `{song_title_query}`"
+        return emojize(
+            f":warning: wtf kind of song is `{song_title_query}` :warning:", use_aliases=True
+        )
     except LookupError as e:
         LOGGER.error(f"LookupError error while searching for song: `{e}`")
         return emojize(
