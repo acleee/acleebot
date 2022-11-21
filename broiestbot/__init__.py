da6d56f2b17b2d1f27dee3f0eb97b8c2c5da52ef
Todd Birchard <toddbirchard@gmail.com>
2022-11-20 20:43:07 -0500

    Update footy configs.

--- broiestbot/__init__.py
+++ broiestbot/__init__.py
@@ -36,7 +36,7 @@ def start_bot():
         print("Starting in dev mode...")
         join_rooms([CHATANGO_TEST_ROOM])
     else:
-        options = {"statsd_host": "127.0.0.1", "statsd_port": 8125}
-        initialize(**options)
+        # options = {"statsd_host": "127.0.0.1", "statsd_port": 8125}
+        # initialize(**options)
         print(f'Joining {", ".join(CHATANGO_ROOMS)}')
         join_rooms(CHATANGO_ROOMS)
