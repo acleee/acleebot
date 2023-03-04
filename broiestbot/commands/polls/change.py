8ff99e9b3f60d4663124fa3b053c2bbcc5e11528
Todd Birchard <toddbirchard@gmail.com>
2023-03-04 17:59:37 -0500

    Add UFC chatango room.

--- broiestbot/commands/polls/change.py
+++ broiestbot/commands/polls/change.py
@@ -1,6 +1,4 @@
 """Conduct a chat poll whether to 'change or stay'."""
-from datetime import datetime, timedelta
-
 from emoji import emojize
 from redis.exceptions import RedisError
 
@@ -19,32 +17,19 @@ def change_or_stay_vote(user_name: str, vote: str) -> str:
     :returns: str
     """
     try:
-        change_votes = r.smembers("change")
-        stay_votes = r.smembers("stay")
-        if len(change_votes) == 0 or len(stay_votes) == 0:
+        votes_to_change = r.smembers("change")
+        votes_to_stay = r.smembers("stay")
+        if not votes_to_change or not votes_to_stay:
             r.sadd("change", "")
             r.sadd("stay", "")
             r.sadd(vote, user_name)
             r.expire("change", 60)
             r.expire("stay", 60)
-            # r.enqueue(datetime.now() + timedelta(seconds=59), poll_results)
-            return emojize(
-                f"\n\n"
-                f":television: <b>CHANGE OR STAY!</b>\n"
-                f"@{user_name} just started a poll and voted to <b>{vote}</b>.\n"
-                f"Voting ends in 60 seconds.",
-                language="en",
-            )
+            # r.enqueue(datetime.now() + timedelta(seconds=60), poll_results)
         elif r.sismember("change", user_name) or r.sismember("stay", user_name):
             return f":warning: pls @{user_name} u already voted :warning:"
         r.lpush(vote, user_name)
-        return emojize(
-            f"\n\n"
-            f"<b>CHANGE:</b> {change_votes}\n"
-            f"<b>STAY:</b> {stay_votes}\n"
-            f":alarm_clock: REMAINING: {r.ttl(vote)}",
-            language="en",
-        )
+        return poll_announcement(user_name, vote)
     except RedisError as e:
         LOGGER.error(f"RedisError while saving 'change or stay' vote from @{user_name}: {e}")
         return emojize(
@@ -57,6 +42,24 @@ def change_or_stay_vote(user_name: str, vote: str) -> str:
             f":warning: my b @{user_name}, broughbert just broke like a littol BITCH :warning:",
             language="en",
         )
+
+
+def poll_announcement(user_name: str, vote: str) -> str:
+    """
+    Initialize a live 'change or stay' poll.
+
+    :param str user_name: Name of user submitting a vote.
+    :param str vote: User's submitted vote (either 'change' or 'stay').
+
+    :returns: str
+    """
+    return emojize(
+        f"\n\n"
+        f":television: <b>CHANGE OR STAY!</b>\n"
+        f"@{user_name} just started a poll and voted to <b>{vote}</b>.\n"
+        f"Voting ends in 60 seconds.",
+        language="en",
+    )
 
 
 def poll_results():
